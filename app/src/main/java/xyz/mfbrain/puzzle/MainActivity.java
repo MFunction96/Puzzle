b0cfba0050589ae44a05ec1d54c9e130417d57d1
MFunction <MFunction@cugb.edu.cn>
2017-07-11 09:34:09 +0800

    布局修改为全屏模式 修正图片分割不均匀的问题 调整游戏界面UI布局

--- app/src/main/java/xyz/mfbrain/puzzle/MainActivity.java
+++ app/src/main/java/xyz/mfbrain/puzzle/MainActivity.java
@@ -12,6 +12,8 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.GridView;
@@ -53,6 +55,9 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);  //隐藏标题栏
+        //设置全屏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         Init();
         _gridview.setAdapter(_ia);
--- app/src/main/java/xyz/mfbrain/puzzle/PreloadActivity.java
+++ app/src/main/java/xyz/mfbrain/puzzle/PreloadActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 /**
@@ -17,6 +19,9 @@ public class PreloadActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);  //隐藏标题栏
+        //设置全屏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_preload);
         Button _startgame = (Button) findViewById(R.id.startgame);
         _startgame.setOnClickListener(new StartGame());
