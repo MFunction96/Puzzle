807587cbd448e5d7a10c94c51608ad347abe4d3c
Maedusaxin <1580208474@qq.com>
2017-07-12 11:42:10 +0800

    自定义对话框

---
+++ app/src/main/java/xyz/mfbrain/puzzle/MyDialog.java
@@ -0,0 +1,74 @@
+package xyz.mfbrain.puzzle;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.StyleRes;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.widget.Button;
+
+/**
+ * Created by Lisa on 2017/7/12.
+ */
+
+public class MyDialog extends Dialog{
+    public Context context;
+    public View dialogView;
+
+    public MyDialog(@NonNull Context context) {
+        super(context);
+        this.context=context;
+    }
+
+    public MyDialog(@NonNull Context context, @StyleRes int themeResId) {
+        super(context, themeResId);
+        this.context=context;
+    }
+
+    protected MyDialog(@NonNull Context context, boolean cancelable, @Nullable OnCancelListener cancelListener) {
+        super(context, cancelable, cancelListener);
+        this.context=context;
+    }
+
+    public void donghua(){
+        AnimationSet animationSet=new AnimationSet(true);
+        RotateAnimation rotateAnimation=new RotateAnimation(0,360, Animation.RELATIVE_TO_SELF,0.5f,
+                Animation.RELATIVE_TO_SELF,0.5f);
+        rotateAnimation.setDuration(2000);
+        animationSet.addAnimation(rotateAnimation);
+        dialogView.startAnimation(rotateAnimation);
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        dialogView= View.inflate(context,R.layout.dialog_info,null);
+        setContentView(dialogView);
+
+        setCanceledOnTouchOutside(false);//点击边缘对话框不消失
+
+        Window window = getWindow();
+        window.setGravity(Gravity.CENTER);  //此处可以设置dialog显示的位置
+        window.setWindowAnimations(R.style.myStyle);  //添加动画
+
+
+        Button bt_sure=(Button)dialogView.findViewById(R.id.btSure);
+        bt_sure.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+            }
+        });
+
+
+    }
+}
