2f1548a795c9cddd35985a907e9afce24842b2d4
1065283526 <1065283526@qq.com>
2017-07-13 14:21:08 +0800

    闯关模式的主界面，还没开始写

---
+++ app/src/main/java/xyz/mfbrain/puzzle/AdventureMode.java
@@ -0,0 +1,13 @@
+package xyz.mfbrain.puzzle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdventureMode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adventuremode);
+    }
+}
