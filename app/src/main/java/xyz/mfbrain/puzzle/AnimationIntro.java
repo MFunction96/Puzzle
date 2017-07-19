12b2c83aaf476aa0ab88ebf430905bf8a14229c6
Maedusaxin <1580208474@qq.com>
2017-07-19 10:21:08 +0800

    开场视频

--- app/src/main/java/xyz/mfbrain/puzzle/AnimationIntro.java
+++ app/src/main/java/xyz/mfbrain/puzzle/AnimationIntro.java
@@ -3,6 +3,8 @@ package xyz.mfbrain.puzzle;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
@@ -14,21 +16,27 @@ import android.widget.ImageView;
  */
 
 public class AnimationIntro extends Activity {
+    MyVideoView myVideoView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_introduction);
-        ImageView imageView = (ImageView) findViewById(R.id.iv);
-        final AnimationDrawable animationDrawable = (AnimationDrawable) imageView.getBackground();
-        animationDrawable.start();
 
-        Button button = (Button) findViewById(R.id.bt_skip);
-        button.setOnClickListener(new View.OnClickListener() {
+        myVideoView=(MyVideoView)findViewById(R.id.myVideo);
+        //设置播放加载路径
+        myVideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.donghua));
+        myVideoView.start();
+
+        myVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(AnimationIntro.this, PreloadActivity.class);
+            public void onCompletion(MediaPlayer mp) {
+                Intent intent=new Intent(AnimationIntro.this,PreloadActivity.class);
                 startActivity(intent);
             }
         });
+
+
+
     }
 }
